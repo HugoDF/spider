9b4dce44be7d9ba04cd7e4dceb36f44fc18006f5
360Edgee <james.edge.13@ucl.ac.uk>
2017-04-08 18:08:30 +0100

    Change to CS, remove metallic and collections

--- scrape.py
+++ scrape.py
@@ -1,7 +1,4 @@
-import scrapy
-import sqlite3
-import re
-import sys
+import scrapy, sqlite3, re, sys
 conn = sqlite3.connect('urls.db')
 
 from w3lib.html import remove_tags, remove_tags_with_content, replace_escape_chars
@@ -13,7 +10,7 @@ conn.commit()
 
 class UclSpider(scrapy.Spider):
 	name = 'Ucl Spider'
-	start_urls = [ 'https://www.ucl.ac.uk' ]
+	start_urls = [ 'http://www.cs.ucl.ac.uk/home/' ]
 
 	def parse(self, response):
 		for html in response.css('body'):
@@ -28,18 +25,21 @@ class UclSpider(scrapy.Spider):
 		links = ""
 		for link in soup.find_all('a'):
     			link_url = link.get('href')
-    			if "ucl.ac.uk" in link_url:
-    					print(link_url)
+    			if "cs.ucl.ac.uk" in link_url:
     					links = links + " " + link_url
 		
-		if "ucl.ac.uk" in response.url:
+		if "cs.ucl.ac.uk" in response.url:
 			c.execute('INSERT INTO urls (url, content, links) VALUES (?, ?, ?)', [ response.url, text.lower(), links ])
 			conn.commit()
 			print (response.url)
 
 		next_page = response.css('a ::attr(href)').extract()
 		for page in next_page:
-			if "ucl.ac.uk" in page:
+			if "metalib" in page:
+				continue
+			if "digital-collections" in page:
+				continue
+			if "cs.ucl.ac.uk" in page:
 				c.execute('SELECT COUNT(*) FROM urls WHERE url = ?', [ page ])
 				urlCount = c.fetchone()
 				if urlCount != 0:
