d32e31a5d580bd2194f822bca8bda9d4b9d5b7cf
Alvin Law K.L <kwoklunlaw@hotmail.com>
2017-04-08 17:43:01 +0100

    Changes for PageRank

--- scrape.py
+++ scrape.py
@@ -1,9 +1,11 @@
 import scrapy
 import sqlite3
 import re
+import sys
 conn = sqlite3.connect('urls.db')
 
 from w3lib.html import remove_tags, remove_tags_with_content, replace_escape_chars
+from bs4 import BeautifulSoup
 
 c = conn.cursor()
 c.execute('DELETE FROM urls')
@@ -16,21 +18,29 @@ class UclSpider(scrapy.Spider):
 	def parse(self, response):
 		for html in response.css('body'):
 			content = html.extract()
+			soup = BeautifulSoup(content, "lxml")
 
 		text = replace_escape_chars(remove_tags(remove_tags_with_content(content, ('script', ))))
 		text = text.replace(u'\xa0', u' ')
 		text = text.replace(u'\xa9', u' ')
 		text = text.replace(u'\\u"\d+"', u' ')
+
+		links = ""
+		for link in soup.find_all('a'):
+    			link_url = link.get('href')
+    			if "ucl.ac.uk" in link_url:
+    					print(link_url)
+    					links = links + " " + link_url
 		
 		if "ucl.ac.uk" in response.url:
-			c.execute('INSERT INTO urls (url, content) VALUES (?, ?)', [ response.url, text.lower() ])
-		 	conn.commit()
-			print response.url
+			c.execute('INSERT INTO urls (url, content, links) VALUES (?, ?, ?)', [ response.url, text.lower(), links ])
+			conn.commit()
+			print (response.url)
 
 		next_page = response.css('a ::attr(href)').extract()
 		for page in next_page:
 			if "ucl.ac.uk" in page:
 				c.execute('SELECT COUNT(*) FROM urls WHERE url = ?', [ page ])
 				urlCount = c.fetchone()
-	     		if urlCount != 0:
-	     			yield scrapy.Request(response.urljoin(page), callback=self.parse)
+				if urlCount != 0:
+					yield scrapy.Request(response.urljoin(page), callback=self.parse)
