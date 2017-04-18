0f6e554bc3440e4944d30fe105e17036287a4148
HugoDF <hdifrancesco@gmail.com>
2017-04-18 23:17:14 +0100

    Move incomingLinks to a separate table

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ Need sqlite3, python 2.7 and pip
 
 Run `sqlite3 urls.db`, run this query to create the urls table:
 ```sql
-CREATE TABLE urls (url STRING, content TEXT, links STRING, incomingLinks STRING, pageRank REAL, amount INTEGER);
+CREATE TABLE urls (url STRING, content TEXT, links STRING, pageRank REAL, amount INTEGER);
 CREATE INDEX contentIndex ON urls (content);
+CREATE TABLE incomingLinks (urlId INTEGER, incomingLinks STRING);
+CREATE INDEX incomingLinksUrlId on incomingLinks (urlId);
 ```
 
 Run `pip install scrapy`.
@@ -17,5 +19,7 @@ To start the spider:
 
 To enable PageRank:
     Generate incoming links for the database: `python makeIncomingLinks.py`
+    To get incomingLinks, you must use something along the lines of: 
+    `SELECT incomingLinks FROM urls LEFT JOIN incomingLinks ON urls.rowid = incomingLinks.urlId`
     Update the database: `python updateDatabase.py`
     Run the algorithm: `python pageRank2.py`
--- makeIncomingLinks.py
+++ makeIncomingLinks.py
@@ -2,12 +2,13 @@ import scrapy, sqlite3, re, sys
 conn = sqlite3.connect('urls.db')
 
 c = conn.cursor()
-query = c.execute("SELECT url FROM urls")
+query = c.execute("SELECT rowid, url FROM urls")
 urls = query.fetchall()
 print "Got " + str(len(urls)) + " urls"
-for url in map(lambda x: x[0], urls):
-    q = c.execute("SELECT url FROM urls WHERE links LIKE '%" + url + "%'")
-    incomingLinks = map(lambda x: x[0], q.fetchall())
-    incomingLinksString = " ".join(incomingLinks)
-    c.execute("INSERT INTO urls (incomingLinks) VALUES (?)", [ incomingLinksString ])
-    conn.commit()
+for url in urls:
+    if url[1] is not None:
+        q = c.execute("SELECT url FROM urls WHERE links LIKE '%" + url[1] + "%'")
+        incomingLinks = map(lambda x: x[0], q.fetchall())
+        incomingLinksString = " ".join(incomingLinks)
+        c.execute("INSERT INTO incomingLinks (urlId, incomingLinks) VALUES (?, ?)", [ url[0], incomingLinksString ])
+        conn.commit()
