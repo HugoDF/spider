eed4567368e975f3c03c31154917a3bad1eb16ed
HugoDF <hdifrancesco@gmail.com>
2017-04-19 09:17:12 +0100

    Add progress indicator. Store rowids instead of URLs. Use prepared statement instead of string concatenation to make query.

--- makeIncomingLinks.py
+++ makeIncomingLinks.py
@@ -4,11 +4,14 @@ conn = sqlite3.connect('urls.db')
 c = conn.cursor()
 query = c.execute("SELECT rowid, url FROM urls")
 urls = query.fetchall()
-print "Got " + str(len(urls)) + " urls"
-for url in urls:
+urlCount = len(urls)
+print "Got " + str(urlCount) + " urls"
+for idx, url in enumerate(urls):
+    if (idx + 1) % 50 == 0:
+        print str(idx + 1) + " / " + str(urlCount)
     if url[1] is not None:
-        q = c.execute("SELECT url FROM urls WHERE links LIKE '%" + url[1] + "%'")
-        incomingLinks = map(lambda x: x[0], q.fetchall())
+        q = c.execute("SELECT rowid FROM urls WHERE links LIKE ?", ('%' + url[1] + '%',))
+        incomingLinks = map(lambda x: str(x[0]), q.fetchall())
         incomingLinksString = " ".join(incomingLinks)
         c.execute("INSERT INTO incomingLinks (urlId, incomingLinks) VALUES (?, ?)", [ url[0], incomingLinksString ])
         conn.commit()
