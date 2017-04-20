ded82a00a4c789ebd97516d7005f4aac120f392b
Alvin Law K.L <kwoklunlaw@hotmail.com>
2017-04-20 16:40:45 +0100

    Remove Old PageRank

--- pageRank.py
+++ pageRank.py
@@ -1,109 +1,103 @@
 import sqlite3
-import numpy as np
+import numpy
 conn = sqlite3.connect('urls.db')
 c = conn.cursor()
 
-def pageRank(graph_matrix, damping_factor = 0.85, max_error = 0.00001):
-    M = graph_matrix
-    #M = stochasticMatrix(graph_matrix)
+class graph:
+  def __init__(self,n):
+    self.size = n
+    self.in_links = {}
+    self.num_out_links = {}
+    self.dead_end = {}
 
-    num_pages = len(graph_matrix)
-    ranking = np.ones(num_pages)
-    constant = np.repeat((1.0 - damping_factor)/num_pages,num_pages)
-    initial = damping_factor * M + constant
+    for j in range(n):
+      self.in_links[j] = []
+      self.num_out_links[j] = 0
+      self.dead_end[j] = True
 
-    old_ranking = np.zeros(num_pages) + 1.0/num_pages
-    while True:
-        ranking = np.dot(initial,old_ranking)
-        dist = distance(ranking, old_ranking)
-        print('Difference: ' + str(dist))
-        if dist < max_error:
-            break
-        else:
-            old_ranking = ranking
+def updateRanking(graph,r,d = 0.85):
+  # graph - graph representing relation between pages
+  # r - PageRank
+  # d - damping factor
 
-    return ranking
+  # Compute the ranking of each page in the graph
+  total_pages = graph.size
+  v = numpy.matrix(numpy.zeros((total_pages,1)))
+  inner_product = sum([r[i] for i in graph.dead_end.keys()])
+  for j in range(total_pages):
+    a = sum([r[k]/graph.num_out_links[k] for k in graph.in_links[j]])
+    v[j] = d*(a+inner_product/total_pages)+(1-d)/total_pages
 
-def stochasticMatrix(graph_matrix):
-    num_pages = len(graph_matrix)
+  return v/numpy.sum(v)
 
-    # Count number of outlinks for each page
-    d = np.zeros(num_pages)
-    for i in range(num_pages):
-        for j in range(num_pages):
-            if (graph_matrix[j, i] == 1):
-                d[i] += 1
+def pagerank(graph, d = 0.85, max_error = 0.00001):
+  # graph - graph representing relation between pages
+  # d - damping factor
+  # max_error - Stop when difference between each iteration less than this value
+  
+  n = graph.size
+  ranking = numpy.matrix(numpy.ones((n,1)))/n
+  iteration = 0
+  dist = 1
+  while dist > max_error:
+    iteration += 1
+    print ("Iteration: %s" % iteration)
+    new_ranking = updateRanking(graph,ranking,d)
+    dist = numpy.sum(numpy.abs(ranking-new_ranking))
+    print ("Difference: %s" % dist)
+    ranking = new_ranking
+  return ranking
 
-        if d[i] == 0:   # dead end, assume go to all links
-            d[i] = num_pages
-
-    # construct stochastic M
-    M = np.zeros((num_pages, num_pages))
-    for i in range(num_pages):
-        if (d[i] == num_pages):  # dead end
-            for j in range(num_pages):
-                M[j, i] = 1.0 / d[i]
-        else:
-            for j in range(num_pages):
-                if graph_matrix[j, i] == 1:
-                    M[j, i] = 1.0 / d[i]
+def graphFromDB():
+    # Construct a graph representing the relation between pages in database
     
-    return M
-
-def distance(v1, v2):
-    v = v1 - v2
-    v = v * v
-    return np.sum(v)
-
-def graphMatrix():
-    c.execute('SELECT COUNT(*) FROM urls')
+    c.execute('SELECT COUNT(*) FROM incomingLinks')
     total_pages = c.fetchall()[0][0]
-    matrix = np.zeros((total_pages, total_pages))
 
-    c.execute('SELECT rowid, links FROM urls')
-    data = c.fetchall()
-    count = 0
-    for element in data:
-        count += 1
-        print("Processing: " + str(count) + "/" + str(total_pages))
-        i = element[0] - 1
-        outlinks = element[1].split()
+    graphDB = graph(total_pages)
 
-        matrix = updateMatrix(matrix,outlinks,i)
-    
-    return matrix
+    # Get incoming links of each page
+    c.execute('SELECT * FROM incomingLinks')
+    datas = c.fetchall()
 
-def updateMatrix(matrix, outlinks, i):
-    if len(outlinks) > 500:
-        matrix = updateMatrix(matrix,outlinks[0:500],i)
-        matrix = updateMatrix(matrix,outlinks[501:(len(outlinks)-1)],i)
-    else:
-        # Find rowid for each outlink
-        query = "SELECT rowid from urls WHERE url IN ({seq})".format(seq=','.join(['?']*len(outlinks)))
-        c.execute(query, outlinks)
-        positions = c.fetchall()
-        for position in positions:
-            j = position[0] - 1
-            matrix[j][i] = 1
-    
-    return matrix
+    count = 0;
+    for data in datas:
+      count += 1;
+      print ("Processing: %s/%s" %(count,total_pages))
 
-def insertPageRankDB(ranking):
-    num_pages = len(ranking)
-    for i in range(num_pages):
-        print("Inserting: " + str(i) + "/" + str(num_pages))
-        rank = ranking[i]
-        c.execute('UPDATE urls SET pageRank = ? WHERE rowid = ?', [ rank, i+1 ])
-        conn.commit()
+      # Database first position is 1, graph first position is 0
+      # Shift by -1
+      position = data[0] - 1
+      if isinstance(data[1],int):
+            in_links = [data[1] - 1]
+      else:
+            in_links = map(int, data[1].split())
+            in_links = [x - 1 for x in in_links]
 
+      graphDB.in_links[position] = in_links
 
-if __name__=='__main__':    
+      for in_link in in_links:
+            if graphDB.num_out_links[in_link] == 0:
+                  graphDB.dead_end.pop(in_link)
+            
+            graphDB.num_out_links[in_link] += 1
 
-    print('Constructing matrix from database...')
-    matrix = graphMatrix()
-    stochasticMatrix(matrix)
-    #print('Computing PageRank...')
-    #ranking = pageRank(matrix)
-    #print('Inserting PageRank into database')
-    #insertPageRankDB(ranking)
-    print('Done!')
+    return graphDB
+
+def insertDB(ranking):
+      total_pages = len(ranking)
+      count = 0
+
+      for i in range(0,total_pages):
+            count += 1
+            print('Inserting: %s/%s' %(count,total_pages))
+            c.execute('UPDATE urls SET pageRank = ? WHERE rowid = ?', [ ranking[i,0], i+1 ])
+            conn.commit()
+
+print('Constructing graph...')
+graphDB = graphFromDB()
+print('Calculating ranking...')
+ranking = pagerank(graphDB)
+print('Inserting into database...')
+insertDB(ranking)
+print('Done!')
